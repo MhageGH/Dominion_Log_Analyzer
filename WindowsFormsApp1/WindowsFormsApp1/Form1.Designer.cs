3b306b414a44ee60a9cc075e72413526a5923e8e
Mhage <masahage@soleil.ocn.ne.jp>
2019-09-28 17:45:08 +0900

    Create WindowsFormsApp

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_log = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_ownCard0 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_name1 = new System.Windows.Forms.Label();
+            this.label_name0 = new System.Windows.Forms.Label();
+            this.label_ownCard1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_log
+            // 
+            this.textBox_log.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox_log.Location = new System.Drawing.Point(12, 30);
+            this.textBox_log.Multiline = true;
+            this.textBox_log.Name = "textBox_log";
+            this.textBox_log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_log.Size = new System.Drawing.Size(387, 478);
+            this.textBox_log.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(94, 514);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ログの解析";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ログを貼り付けて下さい";
+            // 
+            // label_ownCard0
+            // 
+            this.label_ownCard0.AutoSize = true;
+            this.label_ownCard0.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label_ownCard0.Location = new System.Drawing.Point(6, 39);
+            this.label_ownCard0.Name = "label_ownCard0";
+            this.label_ownCard0.Size = new System.Drawing.Size(13, 18);
+            this.label_ownCard0.TabIndex = 3;
+            this.label_ownCard0.Text = "-";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label_name1);
+            this.groupBox1.Controls.Add(this.label_name0);
+            this.groupBox1.Controls.Add(this.label_ownCard1);
+            this.groupBox1.Controls.Add(this.label_ownCard0);
+            this.groupBox1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.groupBox1.Location = new System.Drawing.Point(405, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(266, 496);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "所持カード";
+            // 
+            // label_name1
+            // 
+            this.label_name1.AutoSize = true;
+            this.label_name1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label_name1.Location = new System.Drawing.Point(127, 21);
+            this.label_name1.Name = "label_name1";
+            this.label_name1.Size = new System.Drawing.Size(13, 18);
+            this.label_name1.TabIndex = 7;
+            this.label_name1.Text = "-";
+            // 
+            // label_name0
+            // 
+            this.label_name0.AutoSize = true;
+            this.label_name0.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label_name0.Location = new System.Drawing.Point(6, 21);
+            this.label_name0.Name = "label_name0";
+            this.label_name0.Size = new System.Drawing.Size(13, 18);
+            this.label_name0.TabIndex = 6;
+            this.label_name0.Text = "-";
+            // 
+            // label_ownCard1
+            // 
+            this.label_ownCard1.AutoSize = true;
+            this.label_ownCard1.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label_ownCard1.Location = new System.Drawing.Point(127, 39);
+            this.label_ownCard1.Name = "label_ownCard1";
+            this.label_ownCard1.Size = new System.Drawing.Size(13, 18);
+            this.label_ownCard1.TabIndex = 5;
+            this.label_ownCard1.Text = "-";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.groupBox2.Location = new System.Drawing.Point(677, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(266, 496);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "山札";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(127, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(6, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(127, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(6, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 552);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_log);
+            this.Name = "Form1";
+            this.Text = "Dominion Log Analyzer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_log;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_ownCard0;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label_ownCard1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_name1;
+        private System.Windows.Forms.Label label_name0;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ログ1行から目的語のカードの名前と枚数を抽出
+        private (string name, int num) ExtractObjectCard(string line, string shortPlayerName)
+        {
+            var s = line.Substring((shortPlayerName + "は").Length);
+            s = s.Remove(s.IndexOf("を"));                                            // 例：文字列 "金貨"、文字列 "金貨3枚"
+            var n = System.Text.RegularExpressions.Regex.Replace(s, @"[^0-9]", "");   // 数字を抽出
+            if (n == "") return (s, 1);                                               // 枚数がない->1枚
+            else return (s.Remove(s.IndexOf(n)), Convert.ToInt32(n));
+        }
+
+        private Dictionary<string, int>[] GetOwnCards(string[] log, string[] shortPlayerNames)
+        {
+            string[] get_string = new string[] { "を受け取った。", "を購入・獲得した。", "を獲得した。" };    // TODO トラベラー、仮面、手札に獲得、山札に獲得
+            string discard_string = "を廃棄した。";   // TODO 山札から廃棄 (山札から公開⇒通常廃棄？)
+            string back_string = "の山に戻した。";
+            var ownCards = new Dictionary<string, int>[2] { new Dictionary<string, int>(), new Dictionary<string, int>() };
+            foreach (var line in log)
+            {
+                for (int i = 0; i < 2; ++i)
+                {
+                    for (int j = 0; j < get_string.Length; ++j)
+                    {
+                        if (line.StartsWith(shortPlayerNames[i]) && line.Contains(get_string[j]))
+                        {
+                            var card = ExtractObjectCard(line, shortPlayerNames[i]);
+                            if (ownCards[i].ContainsKey(card.name)) ownCards[i][card.name] += card.num;
+                            else ownCards[i].Add(card.name, card.num);
+                        }
+                    }
+                    if (line.StartsWith(shortPlayerNames[i]) && line.Contains(discard_string))
+                    {
+                        var card = ExtractObjectCard(line, shortPlayerNames[i]);
+                        if (ownCards[i].ContainsKey(card.name)) ownCards[i][card.name] -= card.num;
+                        else MessageBox.Show("Discard failed!");
+                    }
+                    if (line.StartsWith(shortPlayerNames[i]) && line.Contains(back_string))
+                    {
+                        var card = ExtractObjectCard(line, shortPlayerNames[i]);
+                        if (ownCards[i].ContainsKey(card.name)) ownCards[i][card.name] -= card.num;
+                        else MessageBox.Show("back failed!");
+                    }
+                }
+            }
+            return ownCards;
+        }
+
+        // 山札 = 前回のシャフル時のデッキ - シャフル時の手札 - シャフル時の場のカード - シャフル時の脇カード - シャフル時の酒場カード 
+        // - シャフル時以降引いたカード - シャフル時以降山札から捨て札にしたカード  - シャフル時以降山札から廃棄したカード
+        // + シャフル時以降山札に戻したカード + シャフル時以降山札の上に獲得したカード
+        private void ShowDecks()
+        {
+            string bar_string = "を酒場マットに置いた。";
+            string shuffle_string = "山札をシャッフルした。";
+            // TODO
+        }
+
+        // プレイヤ名はターン1開始のログから取得
+        private string[] GetPlayerNames(string[] log)
+        {
+            var playerNames = new List<string>();
+            string turn1_string = "ターン 1 - ";
+            foreach (var line in log)
+            {
+                if (line.Contains(turn1_string))
+                {
+                    playerNames.Add(line.Substring(turn1_string.Length));
+                }
+            }
+            return playerNames.ToArray();
+        }
+
+        // 省略名は名前の先頭から一致しない文字が最初に現れるまでの文字列
+        private string[] GetShortPlayerNames(string[] names)
+        {
+            var shortNames = new string[] { "", "" };
+            for (int i = 0; shortNames[0] == shortNames[1]; ++i)
+            {
+                for (int j = 0; j < names.Length; ++j) if (i < names[j].Length) shortNames[j] += names[j][i];
+            }
+            return shortNames;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var log = textBox_log.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            var playerNames = GetPlayerNames(log);
+            var shortPlayerNames = GetShortPlayerNames(playerNames);
+            var label_names = new Label[2] { label_name0, label_name1 };
+            for (int i = 0; i < 2; ++i) label_names[i].Text = playerNames[i];
+
+            var ownCards = GetOwnCards(log, shortPlayerNames);
+            var label_ownCards = new Label[2] { label_ownCard0, label_ownCard1 };
+            for (int i = 0; i < 2; ++i)
+            {
+                label_ownCards[i].Text = "";
+                foreach (var card in ownCards[i]) if (card.Value != 0) label_ownCards[i].Text += card.Key + " " + card.Value.ToString() + "枚" + Environment.NewLine;
+            }
+            ShowDecks();
+        }
+    }
+}
